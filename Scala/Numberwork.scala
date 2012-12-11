8ce407b103f2492c06c8bc2ef73b3640095b6267
mok4ry <mxm6060@rit.edu>
2012-12-11 15:32:40 -0500

    Added problem 57 in Scala

--- Scala/Numberwork.scala
+++ Scala/Numberwork.scala
@@ -11,10 +11,11 @@ object Numberwork {
     isLychrelNumberRec( n, 0 )
   }
 
+  @deprecated( "Use Numberwork.sumOfDigits instead.", "Dec. 11, 2012" )
   def digitalSum( n: BigInt ): Int = toIntList( n ).foldLeft(0)( _ + _ )
 
   def power( a: Int, b: Int ): BigInt =
-    ( for ( x <- (1 to a) ) yield b ).foldRight(BigInt(1))( _ * _ )
+    ( for ( x <- (1 to b) ) yield a ).foldRight(BigInt(1))( _ * _ )
 
   def isPalindrome( n: BigInt ): Boolean = n == reverseNum( n )
 
@@ -23,5 +24,44 @@ object Numberwork {
 
   def toIntList( n: BigInt ): List[Int] =
     n.toString.toList.map( _.toInt - '0'.toInt )
+
+  def numOfDigits( n: BigInt ): BigInt = sumOfNthPowersOfDigits( n, 0 )
+
+  def sumOfDigits( n: BigInt ): BigInt = sumOfNthPowersOfDigits( n, 1 )
+
+  def sumOfNthPowersOfDigits( n: BigInt, pow: Int ): BigInt =
+    toIntList( n ).map( power( _, pow ) ).foldRight(BigInt(1))( _ + _ )
+
+  def areCoprime( a: BigInt, b: BigInt ): Boolean =
+    greatestCommonFactor( a, b ) == 1
+
+  def greatestCommonFactor( a: BigInt, b: BigInt ): BigInt =
+    if ( b == 0 ) a else greatestCommonFactor( b, a % b )
+
+  def leastCommonMultiple( a: BigInt, b: BigInt ): BigInt =
+    ( a * b ) / greatestCommonFactor( a, b )
+
+  def reduce( a: BigInt, b: BigInt ): ( BigInt, BigInt ) = {
+    val gcf = greatestCommonFactor( a, b )
+    if ( gcf == 1 ) ( a, b ) else reduce( a / gcf, b / gcf )
+  }
+
+  class Rational( num: BigInt, denom: BigInt ) {
+    val reduced = reduce( num, denom )
+    val numerator = reduced._1
+    val denominator = reduced._2
+
+    def + ( that: Rational ): Rational = {
+      val lcm = leastCommonMultiple( denominator, that.denominator )
+      new Rational( lcm / denominator * numerator +
+        lcm / that.denominator * that.numerator, lcm )
+    }
+
+    def / ( that: Rational ): Rational =
+      new Rational(numerator * that.denominator, denominator * that.numerator)
+
+    override def toString: String = numerator + " / " + denominator
+
+  }
 
 }
---
+++ Scala/Problem57.scala
@@ -0,0 +1,53 @@
+// It is possible to show that the square root of two can be expressed as an
+// infinite continued fraction.
+
+// 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+// By expanding this for the first four iterations, we get:
+
+// 1 + 1/2 = 3/2 = 1.5
+// 1 + 1/(2 + 1/2) = 7/5 = 1.4
+// 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+// 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+// The next three expansions are 99/70, 239/169, and 577/408, but the eighth
+// expansion, 1393/985, is the first example where the number of digits in the
+// numerator exceeds the number of digits in the denominator.
+
+// In the first one-thousand expansions, how many fractions contain a numerator
+// with more digits than denominator?
+
+import Numberwork._
+import collection.mutable.HashMap
+
+object Problem57 {
+  val one = new Rational( 1, 1 )
+  val two = new Rational( 2, 1 )
+  val oneHalf = new Rational( 1, 2 )
+  val twoPlusOneHalf = two + oneHalf
+
+  val cache = new HashMap[Int, Rational]
+
+  def main( args: Array[String] ) {
+    val result = for ( x <- (1 to 1000) )
+      yield moreDigsInNum( getNthInterationOfSqrt2( x ) )
+    println( result.count( _ == true ) )
+  }
+
+  def moreDigsInNum( r: Rational ): Boolean =
+    numOfDigits( r.numerator ) > numOfDigits( r.denominator )
+
+  def getNthInterationOfSqrt2( n: Int ): Rational =
+    one + one / getNthInterationOfSqrt2Helper( n )
+
+  def getNthInterationOfSqrt2Helper( n: Int ): Rational = {
+    if ( n == 1 ) two
+    else if ( cache contains (n - 1) ) two + one / cache(n - 1)
+    else {
+      val result = getNthInterationOfSqrt2Helper( n - 1 )
+      cache += n -> ( two + one / result )
+      cache(n)
+    }
+  }
+
+}
